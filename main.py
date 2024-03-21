7d2536db167b4306cf5d4ee202a1fa11d1e62730
toripppppy <Lundy11@icloud.com>
2024-03-21 17:22:30 +0900

    オーバーライドの例を提示

--- main.py
+++ main.py
@@ -21,6 +21,17 @@ keybind_dict = adjust_json.get_keybind_dict()
 
 listener = MIDIListener()
 
+# キーボードに合わせて押した判定の方法をオーバーライドする
+def is_keydown(event: MIDIEvent):
+    return event.status == 144
+
+def is_keyup(event: MIDIEvent):
+    return event.status == 128
+
+listener.is_keydown = is_keydown
+listener.is_keyup = is_keyup
+
+
 @listener.on_keydown
 def keydown(event: MIDIEvent):
     key = keybind_dict.get(event.note)
